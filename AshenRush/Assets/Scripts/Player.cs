bf92be2fb5b24f2b253a4b7ca0f2371e4cad6713
서현경 <hkyung99@naver.com>
2024-12-27 21:46:04 +0900

    add: Player Animation

--- AshenRush/Assets/Scripts/Player.cs
+++ AshenRush/Assets/Scripts/Player.cs
@@ -7,11 +7,14 @@ public class Player : MonoBehaviour
     
     [Header("References")]
     public Rigidbody2D PlayerRigidbody2D;
+    public Animator PlayerAnimator;
+
     private bool isGrounded = true;
 
     void Start()
     {
         PlayerRigidbody2D = transform.GetComponent<Rigidbody2D>();
+        PlayerAnimator = transform.GetComponent<Animator>();
     }
 
     void Update()
@@ -19,11 +22,15 @@ public class Player : MonoBehaviour
         if(Input.GetKeyDown(KeyCode.Space) && isGrounded) {
             PlayerRigidbody2D.AddForceY(JumpForce, ForceMode2D.Impulse);
             isGrounded = false;
+            PlayerAnimator.SetInteger("state", 1);
         }
     }
 
     void OnCollisionEnter2D(Collision2D collision) {
         if (collision.gameObject.tag == "Platform") {
+            if (!isGrounded) {
+                PlayerAnimator.SetInteger("state", 2);
+            }
             isGrounded = true;
         }
     }

01e40dcd4516243593f8e236a11e75f326676577
서현경 <hkyung99@naver.com>
2025-01-20 22:23:05 +0900

    add: Weapon Script

---
+++ AshenRush/Assets/Scripts/Weapon.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+    [Header("References")]
+    public BoxCollider2D WeaponCollider2D;
+    public GameObject Player;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

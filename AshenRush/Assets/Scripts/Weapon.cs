762d49564c92612c716bfd582f2e442fbe755f1c
서현경 <hkyung99@naver.com>
2025-01-25 22:45:13 +0900

    add: Weapon Animation State

--- AshenRush/Assets/Scripts/Weapon.cs
+++ AshenRush/Assets/Scripts/Weapon.cs
@@ -5,12 +5,20 @@ public class Weapon : MonoBehaviour
     [Header("References")]
     public CapsuleCollider2D WeaponCollider2D;
     public Player Player;
+    public Animator WeaponAnimator;
+
+    void Start()
+    {
+        WeaponAnimator = transform.GetComponent<Animator>();
+    }
 
     private void OnEnable() {
+        WeaponAnimator.SetInteger("state", 1);
         Player.StartInvincible();            
     }
 
     private void OnDisable() {
+        WeaponAnimator.SetInteger("state", 0);
         Player.StopInvincible();            
     }
 
